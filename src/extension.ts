faa783fe8c83e322f9014a8f0a786e6f13191d39
FFengIll <fengyouzheng@gmail.com>
2025-09-15 15:35:30 +0800

    feat: new command in `editor/title/context` to control tab & panel.

--- src/extension.ts
+++ src/extension.ts
@@ -9,6 +9,8 @@ export function activate(context: vscode.ExtensionContext) {
     console.log('active: window mode');
 
     var commandId = 'vscode-window-mode.switch';
+    var fullWindowCommandId = 'vscode-window-mode.fullWindow';
+    var showPanelsCommandId = 'vscode-window-mode.showPanels';
 
     // status bar
     statusBarItem = vscode.window.createStatusBarItem(
@@ -29,72 +31,72 @@ export function activate(context: vscode.ExtensionContext) {
 
             current = next;
 
-            switch (name) {
-                case 'LR':
-                    vscode.commands.executeCommand(
-                        'workbench.action.focusSideBar'
-                    );
-                    vscode.commands.executeCommand(
-                        'workbench.action.focusAuxiliaryBar'
-                    );
-                    vscode.commands.executeCommand(
-                        'workbench.action.focusPanel'
-                    );
-                    break;
-                case 'N/A':
-                    vscode.commands.executeCommand(
-                        'workbench.action.closeSidebar'
-                    );
-                    vscode.commands.executeCommand(
-                        'workbench.action.closePanel'
-                    );
-                    vscode.commands.executeCommand(
-                        'workbench.action.closeAuxiliaryBar'
-                    );
-                    break;
-                case 'L':
-                    vscode.commands.executeCommand(
-                        'workbench.action.focusSideBar'
-                    );
-                    vscode.commands.executeCommand(
-                        'workbench.action.closeAuxiliaryBar'
-                    );
-                    vscode.commands.executeCommand(
-                        'workbench.action.closePanel'
-                    );
-                    break;
-                case 'R':
-                    vscode.commands.executeCommand(
-                        'workbench.action.closeSideBar'
-                    );
-                    vscode.commands.executeCommand(
-                        'workbench.action.focusAuxiliaryBar'
-                    );
-                    vscode.commands.executeCommand(
-                        'workbench.action.closePanel'
-                    );
-                    break;
-            }
+            handleSwitchMode(name);
         }
     );
     context.subscriptions.push(switchModeDisposal);
 
-    // register some listener that make sure the status bar
-    // item always up-to-date
-    // context.subscriptions.push(
-    //     vscode.window.onDidChangeActiveTextEditor(updateStatusBarItem)
-    // );
-    // context.subscriptions.push(
-    //     vscode.window.onDidChangeTextEditorSelection(updateStatusBarItem)
-    // );
+    // Full Window 命令
+    var fullWindowDisposal = vscode.commands.registerCommand(
+        fullWindowCommandId,
+        async () => {
+            updateStatusBarItem('N/A');
+            current = 2; // N/A在modes中的索引
+            handleSwitchMode('N/A');
+            await vscode.commands.executeCommand('setContext', 'windowModeIsNA', true);
+        }
+    );
+    context.subscriptions.push(fullWindowDisposal);
+
+    // Restore Panels 命令
+    var showPanelsDisposal = vscode.commands.registerCommand(
+        showPanelsCommandId,
+        async () => {
+            // 打开所有panel
+            updateStatusBarItem('LR');
+            current = 1; // LR在modes中的索引
+            handleSwitchMode('LR');
+            await vscode.commands.executeCommand('setContext', 'windowModeIsNA', false);
+        }
+    );
+    context.subscriptions.push(showPanelsDisposal);
+
+    // 初始化 context
+    vscode.commands.executeCommand('setContext', 'windowModeIsNA', false);
 
     updateStatusBarItem('N/A');
 }
 
+
 function updateStatusBarItem(mode: string) {
-    // show
     statusBarItem.text = `Window: ${mode}`;
     statusBarItem.show();
 }
 
-export function deactivate() {}
+// 新增：统一处理模式切换
+function handleSwitchMode(name: string) {
+    switch (name) {
+        case 'LR':
+            vscode.commands.executeCommand('workbench.action.focusSideBar');
+            vscode.commands.executeCommand('workbench.action.focusAuxiliaryBar');
+            vscode.commands.executeCommand('workbench.action.focusPanel');
+            break;
+        case 'N/A':
+            vscode.commands.executeCommand('workbench.action.closeSidebar');
+            vscode.commands.executeCommand('workbench.action.closePanel');
+            vscode.commands.executeCommand('workbench.action.closeAuxiliaryBar');
+            break;
+        case 'L':
+            vscode.commands.executeCommand('workbench.action.focusSideBar');
+            vscode.commands.executeCommand('workbench.action.closeAuxiliaryBar');
+            vscode.commands.executeCommand('workbench.action.closePanel');
+            break;
+        case 'R':
+            vscode.commands.executeCommand('workbench.action.closeSideBar');
+            vscode.commands.executeCommand('workbench.action.focusAuxiliaryBar');
+            vscode.commands.executeCommand('workbench.action.closePanel');
+            break;
+    }
+}
+
+export function deactivate() { }
